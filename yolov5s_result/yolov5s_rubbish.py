439f998926420ce5981f0f5eece3d8a3ad527dd9
Juwan <junhuanchen@qq.com>
2023-02-02 15:08:50 +0800

    Update yolov5s_rubbish.py

--- yolov5s_result/yolov5s_rubbish.py
+++ yolov5s_result/yolov5s_rubbish.py
@@ -1,24 +1,16 @@
-from ax import pipeline
 import time
-import threading
-
-def pipeline_data(threadName, delay):
-    time.sleep(0.2) # wait for pipeline.work() is True
-    for i in range(200):
-        time.sleep(delay)
-        tmp = pipeline.result()
-        if tmp and tmp['nObjSize']:
-            for i in tmp['mObjects']:
-                print(i)
-    pipeline.free() # 400 * 0.05s auto exit pipeline
-
-thread = threading.Thread(target=pipeline_data, args=("Thread-1", 0.05, ))
-thread.start()
-
+from ax import pipeline
 pipeline.load([
-    b'libsample_vin_ivps_joint_vo_sipy.so',
-    b'-p', b'/home/yolov5s_rubbish.json',
-    b'-c', b'2',
+    'libsample_vin_ivps_joint_vo_sipy.so',
+    '-p', '/home/yolov5s_rubbish.json',
+    '-c', '2',
 ])
-
-thread.join() # wait thread exit
+while pipeline.work():
+    time.sleep(0.001)
+    tmp = pipeline.result()
+    if tmp and tmp['nObjSize']:
+        for i in tmp['mObjects']:
+            print(i)
+        # if tmp['nObjSize'] > 10: # try exit
+        #     pipeline.free()
+pipeline.free()
